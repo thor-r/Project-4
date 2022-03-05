6cd63d613f6410ce386d818c2f784f50bc31b9b3
thor-r <twilliamrefoy@gmail.com>
2022-03-05 12:34:33 +0000

    connected front end

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+const Footer = () => {
+    return (
+        <h1>Footer</h1>
+    )
+}
+
+export default Footer
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+// Bootstrap 
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+const Register = () => {
+    return (
+        <h1>"hello I am Register"</h1>
+    )
+}
+
+export default Register
